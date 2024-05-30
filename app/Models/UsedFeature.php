b0be682d67690f5bf6cef3f36a00820e83a00768
malith94batugedara <malithbatugedara@gmail.com>
2024-05-30 23:12:09 +0530

    models,migrations and seeders done

---
+++ app/Models/UsedFeature.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Feature;
+use App\Models\User;
+
+class UsedFeature extends Model
+{
+    use HasFactory;
+
+    protected $table = 'used_features';
+
+    protected $fillable = [
+              'feature_id',
+              'user_id',
+              'credits'
+    ];
+
+    protected function casts() : array
+    {
+        return [
+            'data' => 'array'
+        ];
+    } 
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function feature(){
+        return $this->belongsTo(Feature::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feature;
+use App\Models\Package;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +20,44 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('123456789')
         ]);
+
+        Feature::create([
+            'image' => 'Test User',
+            'route_name' => 'feature1.index',
+            'name' => 'Calculate Sum',
+            'description' => 'Calculate Sum Of Two Numbers',
+            'required_credits' => 1,
+            'active' => true
+        ]);
+
+        Feature::create([
+            'image' => 'Test User',
+            'route_name' => 'feature2.index',
+            'name' => 'Calculate Difference',
+            'description' => 'Calculate Difference Of Two Numbers',
+            'required_credits' => 3,
+            'active' => true
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credit' => 20,
+        ]);
+
+        Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credit' => 100,
+        ]);
+
+        Package::create([
+            'name' => 'Gold',
+            'price' => 50,
+            'credit' => 500,
+        ]);
+
     }
 }
