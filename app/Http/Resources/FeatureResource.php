e33588bee61d3215f76fd47ef328040055f90a81
malith94batugedara <malithbatugedara@gmail.com>
2024-05-31 08:37:36 +0530

    controllers creation done logic done

---
+++ app/Http/Resources/FeatureResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FeatureResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+           'id' => $this->id,
+           'image' => $this->image ?: null,
+           'route_name' => $this->route_name,
+           'name' => $this->name,
+           'description' => $this->description,
+           'required_credits' => $this->required_credits,
+           'active' => $this->active
+        ];
+    }
+}
